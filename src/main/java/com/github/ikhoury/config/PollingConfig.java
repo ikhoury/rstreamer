9cf48698039559cc27bfc17f92c46a761229b30c
Issa Khoury <i_kh@icloud.com>
2019-08-22 11:22:48 +0200

    Add missing hashcode, equals, and toString on config objects.

--- src/main/java/com/github/ikhoury/config/PollingConfig.java
+++ src/main/java/com/github/ikhoury/config/PollingConfig.java
@@ -1,5 +1,7 @@
 package com.github.ikhoury.config;
 
+import java.util.Objects;
+
 public class PollingConfig {
 
     private int batchSize;
@@ -16,5 +18,27 @@ public class PollingConfig {
 
     public int getBatchSizeThreshold() {
         return batchSizeThreshold;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PollingConfig that = (PollingConfig) o;
+        return batchSize == that.batchSize &&
+                batchSizeThreshold == that.batchSizeThreshold;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(batchSize, batchSizeThreshold);
+    }
+
+    @Override
+    public String toString() {
+        return "PollingConfig{" +
+                "batchSize=" + batchSize +
+                ", batchSizeThreshold=" + batchSizeThreshold +
+                '}';
     }
 }
