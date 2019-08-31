45594e8dad10b86cd7f2903f73a05560f00c3fc8
Issa Khoury <i_kh@icloud.com>
2019-08-31 22:58:20 +0200

    Don't throw connection exceptions. Log warning.

--- src/main/java/com/github/ikhoury/driver/Resilience4jReliableBatchPoller.java
+++ src/main/java/com/github/ikhoury/driver/Resilience4jReliableBatchPoller.java
@@ -1,22 +1,29 @@
 package com.github.ikhoury.driver;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.decorators.Decorators;
 import io.github.resilience4j.retry.IntervalFunction;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+import static java.util.Collections.emptyList;
+
 public class Resilience4jReliableBatchPoller extends ReliableBatchPoller {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Resilience4jReliableBatchPoller.class);
+    private static final float TOLERANCE_PERCENTAGE_OF_CONNECTION_EXCEPTIONS = 0.25F;
+    private static final int MAX_RETRY_ATTEMPTS = 3;
 
     private final CircuitBreaker circuitBreaker;
     private final Retry retry;
-    private static final float TOLERANCE_PERCENTAGE_OF_CONNECTION_EXCEPTION = 0.25F;
-    private static final int MAX_RETRY_ATTEMPTS = 3;
 
     public Resilience4jReliableBatchPoller(RedisBatchPoller poller, String queue) {
         super(poller);
@@ -24,14 +31,30 @@ public class Resilience4jReliableBatchPoller extends ReliableBatchPoller {
         this.retry = Retry.of(queue, createRetryConfig());
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public Optional<String> pollForSingleItemFrom(String queue) throws RedisConnectionException {
-        return decorateSupplier(() -> super.pollForSingleItemFrom(queue)).get();
+        Supplier reliableSupplier = decorateSupplier(() -> super.pollForSingleItemFrom(queue));
+        return getResultOrReturnEmpty(reliableSupplier, Optional.<String>empty());
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public List<String> pollForMultipleItemsFrom(String queue, int count) throws RedisConnectionException {
-        return decorateSupplier(() -> super.pollForMultipleItemsFrom(queue, count)).get();
+        Supplier reliableSupplier = decorateSupplier(() -> super.pollForMultipleItemsFrom(queue, count));
+        return getResultOrReturnEmpty(reliableSupplier, emptyList());
+    }
+
+    private <T> T getResultOrReturnEmpty(Supplier<T> supplier, T emptyResult) {
+        try {
+            return supplier.get();
+        } catch (RedisConnectionException connectionException) {
+            LOGGER.warn("Failed to poll for items. Trying again", connectionException);
+        } catch (CallNotPermittedException ignore) {
+
+        }
+
+        return emptyResult;
     }
 
     private <T> Supplier<T> decorateSupplier(Supplier<T> supplier) {
@@ -44,7 +67,7 @@ public class Resilience4jReliableBatchPoller extends ReliableBatchPoller {
     private CircuitBreakerConfig createCircuitBreakerConfig() {
         return CircuitBreakerConfig.custom()
                 .recordExceptions(RedisConnectionException.class)
-                .failureRateThreshold(TOLERANCE_PERCENTAGE_OF_CONNECTION_EXCEPTION)
+                .failureRateThreshold(TOLERANCE_PERCENTAGE_OF_CONNECTION_EXCEPTIONS)
                 .ringBufferSizeInClosedState(MAX_RETRY_ATTEMPTS * 10)
                 .ringBufferSizeInHalfOpenState(MAX_RETRY_ATTEMPTS)
                 .build();
